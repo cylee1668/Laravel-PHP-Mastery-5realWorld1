3b554925e58cd09ced1ba41aabf34a12f93bc536
cylee1668 <cylee1668@gmail.com>
2025-11-10 10:14:53 +0800

    section 31 end, learn @apply in tailwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,18 @@
 </head>
 
 
+{{-- blade-formmater-disable  --}}
+<style type="text/tailwindcss">
+    .btn {
+        @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+    }
+
+    .link {
+        @apply font-medium text-gray-700 underline decoration-pink-500
+    }
+</style>
+{{-- blade-formmater-enable  --}}
+
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
     
     <h1 class="mb-4 text-2xl">@yield('title')</h1>
